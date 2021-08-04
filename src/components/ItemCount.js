b955d24b3c83ad1d8fad43c7bcc7ca659c9377e5
Rodrigo Sosa <rod2008@live.com.ar>
2021-08-04 00:02:54 -0300

    fixed contador y containers

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,18 +1,13 @@
 import { useState } from "react"
 
-const ItemCount = ({ initial, stock, onAdd}) => {
+const ItemCount = ({ initial, stock}) => {
     const [ counter, setCounter ] = useState(initial)
 
-    const increaseCounter = () => (counter < stock) && setCounter(counter + 1) 
+    const decreaseCounter = () => (counter > 0) && setCounter(prevCount => prevCount-1)
+    
+    const increaseCounter = () => (counter < stock) && setCounter(prevCount => prevCount +1) 
 
-    const decreaseCounter = () => (counter > 0) && setCounter(counter - 1)
 
-    //const addToCart = () => (stock > 0) && onAdd(amount)
-    const addToCart = () => {
-        if (stock > 0) {
-            onAdd(counter)
-        }
-    }
 
 
     return (
@@ -24,14 +19,14 @@ const ItemCount = ({ initial, stock, onAdd}) => {
                         text-black bg-gray-200 hover:bg-white"
                     type="button" onClick={decreaseCounter}>-</button>
                 
-                <p className="text-xs opacity-50 mb-2">Stock: {stock}</p>
+                <p className="text-xs opacity-50 mb-2">Stock: {counter}</p>
                 <button className="
                         top-1 left-1
                         rounded px-3 font-bold
                         text-black bg-gray-200 hover:bg-white"
                     type="button" onClick={increaseCounter}>+</button>
             </div>
-            <button onClick={addToCart}>Agregar al carrito</button>
+            <button >Agregar al carrito</button>
 
         </>
     
